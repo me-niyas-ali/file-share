ed7e6f7bba514c439aee9ab83d5d857ff3c20159
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2025-05-23 14:04:15 +0400

    update server.js

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-const express = require("express");
-const http = require("http");
-const socketio = require("socket.io");
-const path = require("path");
-
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketIo(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
 const rooms = {};
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (room) => {
+io.on('connection', (socket) => {
+  let currentRoom = null;
+
+  socket.on('join-room', (room) => {
     socket.join(room);
-    if (!rooms[room]) rooms[room] = [];
+    currentRoom = room;
+
+    rooms[room] = rooms[room] || [];
     rooms[room].push(socket.id);
 
-    io.to(room).emit("update-count", rooms[room].length);
+    io.to(room).emit('update-count', rooms[room].length);
+  });
 
-    socket.on("start-file", (data) => {
-      socket.to(room).emit("start-file", data);
-    });
+  socket.on('disconnect', () => {
+    if (currentRoom && rooms[currentRoom]) {
+      rooms[currentRoom] = rooms[currentRoom].filter(id => id !== socket.id);
+      io.to(currentRoom).emit('update-count', rooms[currentRoom].length);
+    }
+  });
 
-    socket.on("file-chunk", (data) => {
-      socket.to(room).emit("file-chunk", data);
-    });
+  socket.on('start-file', (data) => {
+    socket.to(currentRoom).emit('start-file', data);
+  });
 
-    socket.on("disconnect", () => {
-      for (const r in rooms) {
-        rooms[r] = rooms[r].filter(id => id !== socket.id);
-        io.to(r).emit("update-count", rooms[r].length);
-      }
-    });
+  socket.on('file-chunk', (data) => {
+    socket.to(currentRoom).emit('file-chunk', data);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
